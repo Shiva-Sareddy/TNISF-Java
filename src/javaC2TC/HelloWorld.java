935595bf3a2fbb98c3bee29a7acd98b58d784f29
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-09-05 13:39:41 +0530

    third commit

--- src/javaC2TC/HelloWorld.java
+++ src/javaC2TC/HelloWorld.java
@@ -3,7 +3,9 @@ package javaC2TC;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, Shiva Sareddy!");
+		int age = 21;
+		System.out.println("You are " + age + " Years Old.");
 
 	}
 
