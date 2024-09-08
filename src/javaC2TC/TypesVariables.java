25cf2425b88bd6040e81b374d2b8471cb3d56335
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-09-08 13:17:58 +0530

    First commit

---
+++ src/javaC2TC/TypesVariables.java
@@ -0,0 +1,24 @@
+package javaC2TC;
+
+public class TypesVariables {
+	// Instance Variables
+	int age;
+	int passOutYear;
+	
+	public void studentDetails(){
+		//local Variables
+		int studentID = 23;
+		double gradePoints = 9.25;
+		String studentName = "Shiva Sareddy";
+		System.out.println("The Student Details: \n\n" + "Student Roll Number: " + studentID + "\nStudent Name: " + studentName + "\nAge: " + age + "\nStudent grade points: " + gradePoints + "\nPass out year: "+ passOutYear);
+	}
+
+	public static void main(String[] args) {
+		// Local variables
+		TypesVariables obj = new TypesVariables();
+		obj.age = 21;
+		obj.passOutYear = 2025;
+		obj.studentDetails();
+	}
+
+}
