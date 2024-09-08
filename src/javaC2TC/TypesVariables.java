8b42b06fe62d50f7fd44e6ba86894a70c724a462
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-09-08 13:40:13 +0530

    second commit

--- src/javaC2TC/TypesVariables.java
+++ src/javaC2TC/TypesVariables.java
@@ -4,13 +4,15 @@ public class TypesVariables {
 	// Instance Variables
 	int age;
 	int passOutYear;
+	//Static variable
+	static String branch = "CSE";
 	
 	public void studentDetails(){
 		//local Variables
-		int studentID = 23;
+		int studentID = 1023;
 		double gradePoints = 9.25;
 		String studentName = "Shiva Sareddy";
-		System.out.println("The Student Details: \n\n" + "Student Roll Number: " + studentID + "\nStudent Name: " + studentName + "\nAge: " + age + "\nStudent grade points: " + gradePoints + "\nPass out year: "+ passOutYear);
+		System.out.println("The Student Details: \n\n" + "Student Roll Number: " + studentID + "\nStudent Name: " + studentName + "\nAge: " + age + "\nBranch: " + branch + "\nStudent grade points: " + gradePoints + "\nPass out year: "+ passOutYear);
 	}
 
 	public static void main(String[] args) {
